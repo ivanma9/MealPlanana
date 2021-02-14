83ade587da61a5d1b3b9742cc99d41c8fd061074
Alex Haddad <alexthegreath@gmail.com>
2021-02-13 22:41:51 -0800

    Implement create recipe feature
    - image and author properties of recipe are not currently implemented

--- meal-planana/src/components/create-recipe.component.js
+++ meal-planana/src/components/create-recipe.component.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+
+import Box from '@material-ui/core/Box';
+import ButtonMUI from '@material-ui/core/Button';
+import ChipInput from 'material-ui-chip-input';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { Form } from 'react-bootstrap';
+import Rating from '@material-ui/lab/Rating';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
 
 export default class CreateRecipe extends Component {
@@ -6,19 +15,32 @@ export default class CreateRecipe extends Component {
     super(props);
 
     this.state = {
+      recipe_title: '',
       recipe_description: '',
-      recipe_responsible: '',
-      recipe_priority: '',
-      recipe_completed: false,
+      recipe_ingredients: [],
+      recipe_directions: '',
+      recipe_tags: [],
+      // recipe_image: '',
+      recipe_rating: 0,
+      // recipe_author: '',
     };
-    this.onChangeRecipeDescription = this.onChangeRecipeDescription.bind(
-      this,
-    );
-    this.onChangeRecipeResponsible = this.onChangeRecipeResponsible.bind(
-      this,
-    );
-    this.onChangeRecipePriority = this.onChangeRecipePriority.bind(this);
+    this.onChangeRecipeTitle = this.onChangeRecipeTitle.bind(this);
+    this.onChangeRecipeDescription = this.onChangeRecipeDescription.bind(this);
+    this.onAddRecipeIngredient = this.onAddRecipeIngredient.bind(this);
+    this.onDeleteRecipeIngredient = this.onDeleteRecipeIngredient.bind(this);
+    this.onChangeRecipeDirections = this.onChangeRecipeDirections.bind(this);
+    this.onAddRecipeTag = this.onAddRecipeTag.bind(this);
+    this.onDeleteRecipeTag = this.onDeleteRecipeTag.bind(this);
+    // this.onChangeRecipeImage = this.onChangeRecipeImage.bind(this);
+    this.onChangeRecipeRating = this.onChangeRecipeRating.bind(this);
+    // this.onChangeRecipeAuthor = this.onChangeRecipeAuthor.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeRecipeTitle(e) {
+    this.setState({
+      recipe_title: e.target.value,
+    });
   }
 
   onChangeRecipeDescription(e) {
@@ -27,42 +49,92 @@ export default class CreateRecipe extends Component {
     });
   }
 
-  onChangeRecipeResponsible(e) {
+  onAddRecipeIngredient(chip) {
+    this.setState((prevState) => ({
+      recipe_ingredients: [...prevState.recipe_ingredients, chip],
+    }));
+  }
+
+  onDeleteRecipeIngredient(chip, index) {
+    this.setState((prevState) => ({
+      recipe_ingredients: prevState.recipe_ingredients.filter((_, i) => i !== index),
+    }));
+  }
+
+  onChangeRecipeDirections(e) {
     this.setState({
-      recipe_responsible: e.target.value,
+      recipe_directions: e.target.value,
     });
   }
 
-  onChangeRecipePriority(e) {
+  onAddRecipeTag(chip) {
+    this.setState((prevState) => ({
+      recipe_tags: [...prevState.recipe_tags, chip],
+    }));
+  }
+
+  onDeleteRecipeTag(chip, index) {
+    this.setState((prevState) => ({
+      recipe_tags: prevState.recipe_tags.filter((_, i) => i !== index),
+    }));
+  }
+
+  // onChangeRecipeImage(e) {
+  //   this.setState({
+  //     recipe_image: e.target.value,
+  //   });
+  // }
+
+  onChangeRecipeRating(e) {
     this.setState({
-      recipe_priority: e.target.value,
+      recipe_rating: e.target.value,
     });
   }
+
+  // onChangeRecipeAuthor(e) {
+  //   this.setState({
+  //     recipe_author: e.target.value,
+  //   });
+  // }
 
   onSubmit(e) {
     e.preventDefault();
-
     console.log('Form submitted:');
+    console.log(`Recipe Title: ${this.state.recipe_title}`);
     console.log(`Recipe Description: ${this.state.recipe_description}`);
-    console.log(`Recipe Responsible: ${this.state.recipe_responsible}`);
-    console.log(`Recipe Priority: ${this.state.recipe_priority}`);
+    console.log(`Recipe Ingredients: ${this.state.recipe_ingredients}`);
+    console.log(`Recipe Directions: ${this.state.recipe_directions}`);
+    console.log(`Recipe Tags: ${this.state.recipe_tags}`);
+    // console.log(`Recipe Image: ${this.state.recipe_image}`);
+    console.log(`Recipe Rating: ${this.state.recipe_rating}`);
+    // console.log(`Recipe Author: ${this.state.recipe_author}`);
 
     const newRecipe = {
+      recipe_title: this.state.recipe_title,
       recipe_description: this.state.recipe_description,
-      recipe_responsible: this.state.recipe_responsible,
-      recipe_priority: this.state.recipe_priority,
-      recipe_completed: this.state.recipe_completed,
+      recipe_ingredients: this.state.recipe_ingredients,
+      recipe_directions: this.state.recipe_directions,
+      recipe_tags: this.state.recipe_tags,
+      // recipe_image: this.state.recipe_image,
+      recipe_rating: this.state.recipe_rating,
+      // recipe_author: this.state.recipe_author,
     };
+
+    console.log(newRecipe);
 
     axios
       .post('http://localhost:4000/recipes/add', newRecipe)
       .then((res) => console.log(res.data));
 
     this.setState({
+      recipe_title: '',
       recipe_description: '',
-      recipe_responsible: '',
-      recipe_priority: '',
-      recipe_completed: false,
+      recipe_ingredients: [],
+      recipe_directions: '',
+      recipe_tags: [],
+      // recipe_image: '',
+      recipe_rating: 0,
+      // recipe_author: '',
     });
   }
 
@@ -70,74 +142,117 @@ export default class CreateRecipe extends Component {
     return (
       <div style={{ marginTop: 10 }}>
         <h3>Create New Recipe</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Description: </label>
-            <input
-              type="text"
-              className="form-control"
+
+        <Form>
+
+          {/* Title */}
+          {/* TODO: Implement check to make sure something is entered in the field */}
+          <Form.Group controlID="formGroupRecipeTitle">
+            <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Title</Typography>
+            <TextField
+              variant="outlined"
+              placeholder="Title"
+              value={this.state.recipe_title}
+              onChange={this.onChangeRecipeTitle}
+              style={{ width: '100%' }}
+            />
+          </Form.Group>
+
+          <br />
+
+          {/* Description */}
+          <Form.Group controlID="formGroupRecipeDescription">
+            <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Description</Typography>
+            <TextField
+              multiline
+              variant="outlined"
+              placeholder="Description"
               value={this.state.recipe_description}
               onChange={this.onChangeRecipeDescription}
+              style={{ width: '100%' }}
+              rows={3}
+              rowsMax={15}
             />
-          </div>
-          <div className="form-group">
-            <label>Responsible: </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.recipe_responsible}
-              onChange={this.onChangeRecipeResponsible}
-            />
-          </div>
-          <div className="form-group">
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priorityOptions"
-                id="priorityLow"
-                value="Low"
-                checked={this.state.recipe_priority === 'Low'}
-                onChange={this.onChangeRecipePriority}
-              />
-              <label className="form-check-label">Low</label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priorityOptions"
-                id="priorityMedium"
-                value="Medium"
-                checked={
-                  this.state.recipe_priority === 'Medium'
-                }
-                onChange={this.onChangeRecipePriority}
-              />
-              <label className="form-check-label">Medium</label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priorityOptions"
-                id="priorityHigh"
-                value="High"
-                checked={this.state.recipe_priority === 'High'}
-                onChange={this.onChangeRecipePriority}
-              />
-              <label className="form-check-label">High</label>
-            </div>
-          </div>
+          </Form.Group>
 
-          <div className="form-group">
-            <input
-              type="submit"
-              value="Create Recipe"
-              className="btn btn-primary"
+          <br />
+
+          {/* Ingredients */}
+          <Form.Group controlID="formGroupRecipeIngredients">
+            <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Ingredients</Typography>
+            <ChipInput
+              allowDuplicates={false}
+              // TODO: add "dataSource" array for autocompletion
+              fullWidth
+              value={this.state.recipe_ingredients}
+              onAdd={(chip) => this.onAddRecipeIngredient(chip)}
+              onDelete={(chip, index) => this.onDeleteRecipeIngredient(chip, index)}
             />
+          </Form.Group>
+
+          <br />
+
+          {/* Directions */}
+          {/* TODO: Implement check to make sure something is entered in the field */}
+          <Form.Group controlID="formGroupRecipeDirections">
+            <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Directions</Typography>
+            <TextField
+              multiline
+              variant="outlined"
+              placeholder="Directions"
+              value={this.state.recipe_directions}
+              onChange={this.onChangeRecipeDirections}
+              style={{ width: '100%' }}
+              rows={3}
+              rowsMax={15}
+            />
+          </Form.Group>
+
+          <br />
+
+          {/* Tags */}
+          <Form.Group controlID="formGroupRecipeTags">
+            <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Tags</Typography>
+            <ChipInput
+              allowDuplicates={false}
+              // TODO: add "dataSource" array for autocompletion
+              fullWidth
+              value={this.state.recipe_tags}
+              onAdd={(chip) => this.onAddRecipeTag(chip)}
+              onDelete={(chip, index) => this.onDeleteRecipeTag(chip, index)}
+            />
+          </Form.Group>
+          {/* TODO: add image preview and upload ability */}
+
+          <br />
+
+          {/* Rating */}
+          <Form.Group controlID="formGroupRecipeRating">
+            <Box component="fieldset" mb={3} borderColor="transparent">
+              <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Rating</Typography>
+              <Rating
+                name="hearts"
+                defaultValue={0}
+                value={this.state.recipe_rating}
+                precision={0.5}
+                icon={<FavoriteIcon fontSize="inherit" />}
+                style={{ color: 'red' }}
+                onChange={this.onChangeRecipeRating}
+              />
+            </Box>
+          </Form.Group>
+
+          <br />
+
+          <div style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            display: 'flex',
+          }}
+          >
+            <ButtonMUI variant="contained" color="primary" onClick={this.onSubmit}>Create Recipe</ButtonMUI>
           </div>
-        </form>
+        </Form>
       </div>
     );
   }
