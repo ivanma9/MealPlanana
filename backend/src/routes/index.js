35f5486b42b2b2cc49259f11ef61a5510fd8362d
Bryce Stulman <bstulman@ucla.edu>
2021-02-17 21:04:19 -0800

    Export recipeRoutes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,4 +2,4 @@ import userRoutes from './user';
 import sessionRoutes from './session';
 import recipeRoutes from './recipes';
 
-export { userRoutes, sessionRoutes };
+export { userRoutes, sessionRoutes, recipeRoutes };
