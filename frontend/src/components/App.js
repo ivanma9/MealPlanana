501a679be4b22d8ba0b15ba74e3379ac458a5660
Alex Haddad <alexthegreath@gmail.com>
2021-03-01 14:24:23 -0800

    Make Navbar only display once logged in

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route';
 
 import CreateRecipe from './recipes/CreateRecipe';
@@ -14,21 +14,25 @@ import EditRecipe from './recipes/EditRecipe';
 
 export default () => (
   <>
-    <Navbar />
-    <Route exact path="/" component={Welcome} />
-    <AuthRoute path="/login" component={Login} />
-    <AuthRoute path="/signup" component={Signup} />
-    <ProtectedRoute path="/dashboard" component={Dashboard} />
-    <Route
-      path="/recipes"
-      render={({ match: { url } }) => (
-        <>
-          <ProtectedRoute path={`${url}/`} component={RecipesList} exact />
-          <ProtectedRoute path={`${url}/view/:id`} component={ViewRecipe} />
-          <ProtectedRoute path={`${url}/create`} component={CreateRecipe} />
-          <ProtectedRoute path={`${url}/edit/:id`} component={EditRecipe} />
-        </>
-      )}
-    />
+    <Switch>
+      <Route exact path="/" component={Welcome} />
+      <AuthRoute path="/login" component={Login} />
+      <AuthRoute path="/signup" component={Signup} />
+      <>
+        <Navbar />
+        <ProtectedRoute path="/dashboard" component={Dashboard} />
+        <Route
+          path="/recipes"
+          render={({ match: { url } }) => (
+            <>
+              <ProtectedRoute path={`${url}/`} component={RecipesList} exact />
+              <ProtectedRoute path={`${url}/view/:id`} component={ViewRecipe} />
+              <ProtectedRoute path={`${url}/create`} component={CreateRecipe} />
+              <ProtectedRoute path={`${url}/edit/:id`} component={EditRecipe} />
+            </>
+          )}
+        />
+      </>
+    </Switch>
   </>
 );
