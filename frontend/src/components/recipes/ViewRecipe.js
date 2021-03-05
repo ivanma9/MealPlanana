ivanma9/MealPlanana

commit 46d1f9583551d2da20ef1703dfdfa111e168923f

Update author to new schema

frontend/src/components/recipes/ViewRecipe.js
@@ -1,13 +1,5 @@
 import {
-  Card,
-  CardMedia,
-  Chip,
-  Fab,
-  Grid,
-  Paper,
-  Popover,
-  Snackbar,
-  Typography,
+  Card, CardMedia, Chip, Fab, Grid, Paper, Popover, Snackbar, Typography,
 } from '@material-ui/core';
 import React, { Component } from 'react';
 import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';

frontend/src/components/recipes/CreateRecipe.js
@@ -1,16 +1,14 @@
+import {
+  Button, TextField, Typography,
+} from '@material-ui/core';
 import React, { Component } from 'react';
 
-import Box from '@material-ui/core/Box';
-import ButtonMUI from '@material-ui/core/Button';
 import ChipInput from 'material-ui-chip-input';
 import { Editor } from '@tinymce/tinymce-react';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import { Form } from 'react-bootstrap';
-import Rating from '@material-ui/lab/Rating';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 import { createRecipe } from '../../actions/recipes';
 
 const sanitizeHtml = require('sanitize-html');
@@ -35,8 +33,10 @@ class CreateRecipe extends Component {
       directions: '',
       tags: [],
       // recipe_image: '',
-      ratingTotal: 0,
-      authorID: '',
+      author: {
+        id: '',
+        username: '',
+      },
 
       titleIsEmpty: false,
       // directionsIsEmpty: false,
@@ -50,13 +50,15 @@ class CreateRecipe extends Component {
     this.onAddTag = this.onAddTag.bind(this);
     this.onDeleteTag = this.onDeleteTag.bind(this);
     // this.onChangeRecipeImage = this.onChangeRecipeImage.bind(this);
-    this.onChangeRating = this.onChangeRating.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
     this.setState({
-      authorID: this.props.session.userId,
+      author: {
+        id: this.props.session.userId,
+        username: this.props.session.username,
+      },
     });
   }
 
@@ -115,12 +117,6 @@ class CreateRecipe extends Component {
     this.setState({
       directions: content,
     });
-
-    // if (content.length === 0) {
-    //   this.setState({ directionsIsEmpty: true });
-    // } else {
-    //   this.setState({ directionsIsEmpty: false });
-    // }
   }
 
   onAddTag(chip) {
@@ -141,12 +137,6 @@ class CreateRecipe extends Component {
   //   });
   // }
 
-  onChangeRating(e) {
-    this.setState({
-      ratingTotal: e.target.value,
-    });
-  }
-
   onSubmit() {
     if (!this.handleValidation()) {
       console.log('Form invalid');
@@ -163,8 +153,8 @@ class CreateRecipe extends Component {
     console.log(`Recipe Directions: ${this.state.directions}`);
     console.log(`Recipe Tags: ${this.state.tags}`);
     // console.log(`Recipe Image: ${this.state.recipe_image}`);
-    console.log(`Recipe Rating: ${this.state.ratingTotal}`);
-    console.log(`Recipe Author: ${this.state.authorID}`);
+    console.log(`Recipe Author ID: ${this.state.author.id}`);
+    console.log(`Recipe Author Username: ${this.state.author.username}`);
 
     // TODO: more advanced sanitizing. Error if user trying to input bad data and don't post to db?
     const newRecipe = {
@@ -174,8 +164,11 @@ class CreateRecipe extends Component {
       directions: this.state.directions,
       tags: this.state.tags,
       // recipe_image: this.state.recipe_image,
-      ratingTotal: this.state.ratingTotal,
-      author: this.state.authorID,
+      ratingTotal: 0,
+      author: {
+        id: this.state.author.id,
+        username: this.state.author.username,
+      },
     };
 
     console.log(newRecipe);
@@ -187,8 +180,10 @@ class CreateRecipe extends Component {
       directions: '',
       tags: [],
       // recipe_image: '',
-      ratingTotal: 0,
-      authorID: '',
+      author: {
+        id: '',
+        username: '',
+      },
     });
 
     this.props.createRecipe(newRecipe)
@@ -321,31 +316,13 @@ class CreateRecipe extends Component {
 
           <br />
 
-          {/* Rating */}
-          <Form.Group controlid="formGroupRecipeRating">
-            <Box component="fieldset" mb={3} borderColor="transparent">
-              <Typography variant="button" component="legend" className="mb-2" style={{ fontSize: 18 }}>Rating</Typography>
-              <Rating
-                name="hearts"
-                defaultValue={0}
-                value={this.state.ratingTotal}
-                precision={0.5}
-                icon={<FavoriteIcon fontSize="inherit" />}
-                style={{ color: 'red' }}
-                onChange={this.onChangeRating}
-              />
-            </Box>
-          </Form.Group>
-
-          <br />
-
           <div style={{
             justifyContent: 'center',
             alignItems: 'center',
             display: 'flex',
           }}
           >
-            <ButtonMUI variant="contained" color="primary" onClick={this.onSubmit}>Create Recipe</ButtonMUI>
+            <Button variant="contained" color="primary" onClick={this.onSubmit}>Create Recipe</Button>
           </div>
         </Form>
       </div>

frontend/src/components/recipes/EditRecipe.js
@@ -1,14 +1,8 @@
 /* eslint no-undef: 0 */ // --> OFF
 
 import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField,
-  Typography,
+  Button, Dialog, DialogActions, DialogContent,
+  DialogContentText, DialogTitle, TextField, Typography,
 } from '@material-ui/core';
 import React, { Component } from 'react';
 

frontend/src/components/recipes/RecipesList.js
@@ -7,16 +7,7 @@
 //       reach this page
 
 import {
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Chip,
-  Fab,
-  Grid,
-  IconButton,
-  Snackbar,
-  Typography,
+  Button, Card, CardContent, CardMedia, Chip, Fab, Grid, IconButton, Snackbar, Typography,
 } from '@material-ui/core';
 import React, { Component, useEffect, useState } from 'react';
 
@@ -162,7 +153,9 @@ class RecipesList extends Component {
   constructor(props) {
     super(props);
 
-    const open = this.props.location.appState !== undefined ? this.props.location.appState.open : false;
+    const open = this.props.location.appState !== undefined
+      ? this.props.location.appState.open
+      : false;
 
     this.state = {
       open,
@@ -211,7 +204,8 @@ class RecipesList extends Component {
 
   createMealHandleRecipeUnselected = (recipe) => {
     this.setState((prevState) => ({
-      createMealSelectedRecipes: prevState.createMealSelectedRecipes.filter((item) => item !== recipe),
+      createMealSelectedRecipes:
+        prevState.createMealSelectedRecipes.filter((item) => item !== recipe),
     }));
   };
 
@@ -283,7 +277,7 @@ class RecipesList extends Component {
               }}
             >
               <AddIcon />
-              New
+              New Recipe
             </Fab>
           </Link>
           <Fab
@@ -368,6 +362,9 @@ Recipe.propTypes = {
   onMouseOut: PropTypes.func,
   checkIfCurrentCard: PropTypes.func,
   addRecipeToState: PropTypes.func,
+  createMealPromptIsOpen: PropTypes.bool,
+  createMealHandleRecipeSelected: PropTypes.func,
+  createMealHandleRecipeUnselected: PropTypes.func,
   recipe: PropTypes.shape({
     _id: Schema.Types.ObjectId.isRequired,
     preview: PropTypes.shape({
@@ -385,6 +382,9 @@ Recipe.defaultProps = {
   onMouseOut: () => {},
   checkIfCurrentCard: () => {},
   addRecipeToState: () => {},
+  createMealPromptIsOpen: () => {},
+  createMealHandleRecipeSelected: () => {},
+  createMealHandleRecipeUnselected: () => {},
   recipe: PropTypes.shape({
     preview: null,
     description: '',

commit 05a2afa8d561c84fba9eea17d44d07e6f4c99082

Fix bug: user was unable to edit a recipe they were the author of
introduced in 46d1f9583551d2da20ef1703dfdfa111e168923f

frontend/src/components/recipes/ViewRecipe.js
@@ -75,7 +75,7 @@ class ViewRecipe extends Component {
     const checkIfCurrentCard = (currentID) => this.state.activeID === currentID;
 
     const handleEditButtonClicked = (e) => {
-      if (this.props.userID !== this.props.recipe.author) {
+      if (this.props.userID !== this.props.recipe.author.id) {
         this.setState({
           cannotEditPopoverOpen: true,
           cannotEditPopoverAnchorElement: e.currentTarget,