463065d7dc789d038b0aeba899699d40509ad714
Alex Haddad <alexthegreath@gmail.com>
2021-03-04 22:46:33 -0800

    Add persistent storage of global state
    - refreshing the page now doesn't clear the state
    - can use this in the future to avoid fetching from db too often
    - Bug: author of recipe is incorrectly checked. This means no one can edit a 
      recipe.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,19 @@
 import { applyMiddleware, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
 import thunk from 'redux-thunk';
 import reducer from '../reducers/root';
 
-export default (preloadedState) => createStore(
-  reducer,
-  preloadedState,
-  applyMiddleware(thunk),
-);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(persistedReducer, middleware);
+const persistor = persistStore(store);
+
+export { store, persistor };
