6e531f3836620974a4cc04ecb327f841a6af4404
Ivan <ivanma819@gmail.com>
2021-03-10 17:55:07 -0800

    Update alignment for Navbar
    logout flush right
    nav items flush left

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,88 +19,90 @@ const mapDispatchToProps = (dispatch) => ({
 
 function NavBar({ logout }) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', height: '5em' }}>
-      <Navbar.Brand
-        className="bg-dark"
-        style={{
-          paddingRight: '1em', paddingLeft: '1em', marginRight: '-0.5em',
-        }}
-      >
-        <Link
-          to="/dashboard"
-          className="navbar-brand"
+    <div className="d-flex">
+      <div className="d-flex" style={{ height: '5em' }}>
+        <Navbar.Brand
+          className="bg-dark"
           style={{
-            display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '0.4em',
+            paddingRight: '1em', paddingLeft: '1em', marginRight: '-0.5em',
           }}
         >
-          <Typography
-            variant="button"
+          <Link
+            to="/dashboard"
+            className="navbar-brand"
             style={{
-              fontSize: 30,
-              paddingRight: '0.2em',
-              paddingLeft: '0.5em',
-              color: 'white',
+              display: 'inline', alignItems: 'center', justifyContent: 'center', paddingTop: '0.4em',
             }}
           >
-            Meal Planana
-          </Typography>
-          <img src={BananaBunch} alt="Logo" style={{ width: '2em' }} />
-          {/* <img src={BananaBunchBlackOutline} alt="Logo" style={{ width: '2em' }} /> */}
-        </Link>
-      </Navbar.Brand>
-      <Navbar expand="lg">
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav>
-            <Nav.Link>
-              <Link to="/dashboard" className="nav-link">
-                <Typography
-                  variant="button"
-                  style={{
-                    fontSize: 25,
-                  }}
-                >
-                  Dashboard
-                </Typography>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/recipes" className="nav-link">
-                <Typography
-                  variant="button"
-                  style={{
-                    fontSize: 25,
-                  }}
-                >
-                  Recipes
-                </Typography>
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/recipes/my-recipes" className="nav-link">
-                <Typography
-                  variant="button"
-                  style={{
-                    fontSize: 25,
-                  }}
-                >
-                  My Recipes
-                </Typography>
-              </Link>
-            </Nav.Link>
-          </Nav>
-          <div
-            className="navbar-item ml-auto"
-            style={{
-              display: 'flex', alignItems: 'center', justifyContent: 'center',
-            }}
-          >
-            <Button className="mr-2 p-2" onClick={logout} style={{ backgroundColor: '#709255' }}>
-              Logout
-            </Button>
-          </div>
-        </Navbar.Collapse>
-      </Navbar>
+            <Typography
+              variant="button"
+              style={{
+                fontSize: 30,
+                paddingRight: '0.2em',
+                paddingLeft: '0.5em',
+                color: 'white',
+              }}
+            >
+              Meal Planana
+            </Typography>
+            <img src={BananaBunch} alt="Logo" style={{ width: '2em' }} />
+            {/* <img src={BananaBunchBlackOutline} alt="Logo" style={{ width: '2em' }} /> */}
+          </Link>
+        </Navbar.Brand>
+        <Navbar expand="lg">
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link>
+                <Link to="/dashboard" className="nav-link">
+                  <Typography
+                    variant="button"
+                    style={{
+                      fontSize: 25,
+                    }}
+                  >
+                    Dashboard
+                  </Typography>
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/recipes" className="nav-link">
+                  <Typography
+                    variant="button"
+                    style={{
+                      fontSize: 25,
+                    }}
+                  >
+                    Recipes
+                  </Typography>
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/recipes/my-recipes" className="nav-link">
+                  <Typography
+                    variant="button"
+                    style={{
+                      fontSize: 25,
+                    }}
+                  >
+                    My Recipes
+                  </Typography>
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      <div
+        className="navbar-item ml-auto"
+        style={{
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+        }}
+      >
+        <Button className="mr-3 py-2 px-3" onClick={logout} style={{ backgroundColor: '#709255' }}>
+          Logout
+        </Button>
+      </div>
     </div>
   );
 }
